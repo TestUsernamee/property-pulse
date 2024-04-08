61837ce266b346987f4d752ae4a18dcee35c6b66
Ante <nicevidproductions@gmail.com>
2024-04-08 20:18:27 +0200

    modified:   app/page.jsx 	new file:   app/properties/[id]/page.jsx 	new file:   app/properties/add/page.jsx 	new file:   app/properties/page.jsx

---
+++ app/properties/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+function PropertiesPage() {
+  return (
+    <div>
+      <h1 className="text-3xl">Properties</h1>
+      <Link href="/">GoHome</Link>
+    </div>
+  );
+}
+
+export default PropertiesPage;
